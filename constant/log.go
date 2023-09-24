d7f2b5617db6f01311797554b08b051fa7219ca7
Jean-Baptiste ABET <abet.jean-baptiste@urbasolar.com>
2023-09-24 18:05:10 +0200

    fix: forgotten constant file for logs

---
+++ constant/log.go
@@ -0,0 +1,7 @@
+package constant
+
+const LOG_LEVEL_DEBUG = "DEBUG"
+const LOG_LEVEL_INFO = "INFO"
+const LOG_LEVEL_WARNING = "WARNING"
+const LOG_LEVEL_ERROR = "ERROR"
+const LOG_LEVEL_CRITICAL = "CRITICAL"
